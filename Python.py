7d5e08a2f37d92f344836b87bd49cedda02bb827
Nick-Guez12 <56379863+Nick-Guez12@users.noreply.github.com>
2022-10-25 21:44:25 +1030

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,6 +1,7 @@
 
 # Online Python - IDE, Editor, Compiler, Interpreter
 import pandas as pd
+import numpy as np
 
 df = pd.read_excel('Downloads\2019-20-australian-government-contract-data.xlsx')
 df.head()
@@ -9,9 +10,13 @@ df['Applicable Start Date'] = pd.to_datetime(df['Applicable Start Date'], format
 
 filtered_df = df.loc[(df['Applicable Start Date'] >= '2022-01-01')]
 
+# price as an array not yet implemented
 if filtered_df['Amendments Value'] != '':
     price = filtered_df['Amendments Value']/filtered_df['Duration Years']
 else:
     price = filtered_df['Value']/filtered_df['Duration Years']
     
+np.sort(price)
 
+#Use that to sort filtered_df
+#print filtered_df
